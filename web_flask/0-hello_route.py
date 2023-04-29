d5581af5f2f4d1d14466b57cc51ce53d600c69d0
Yibrah Teare <112482941+yobocode@users.noreply.github.com>
2023-04-29 08:13:50 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
 
+"""Start Flask web application.
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'
@@ -12,7 +12,6 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
